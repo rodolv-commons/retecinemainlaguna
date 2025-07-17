0c006f7527c5c69e3be11172457261dac3a3b369
rodolv1979 <rodolfo.marraffa@github.com>
2025-07-17 22:48:26 +0200

    WIP Implement footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Header from './Header.svelte';
 	import '../styles/app.css';
+	import Footer from './Footer.svelte';
 
 	let { children } = $props();
 </script>
@@ -12,11 +13,9 @@
 		{@render children()}
 	</main>
 
-	<footer>
-		<p>
-			visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to learn about SvelteKit
-		</p>
-	</footer>
+	<div class="bar"></div>
+
+	<Footer />
 </div>
 
 <style>
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { page } from '$app/state';
+  	import '../styles/footer.css';
+</script>
+
+<footer>
+  <div class="footer__wrapper">
+    <div class="footer__infos">Rete Cinema In Laguna</div>
+    <nav class="footer__nav">
+      <ol>
+        <li>retecinemainlaguna@gmail.com</li>      
+        <li>IG</li>
+        <li>IG</li>
+        <li>Facebook</li>
+      </ol>
+    </nav>
+  </div>
+</footer>
